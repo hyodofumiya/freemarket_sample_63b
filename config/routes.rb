aa4c017691826942622ccb669b03e5ca4914b2e9
hyodofumiya <f.hyodo.0123@gmail.com>
2020-03-16 12:39:32 +0900

    routesをgetからresourcesに変更
    reoutesの記述をgetからresourcesを用いた物に変更した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/destroy'
-  get 'brands/index'
-  get 'brands/new'
-  get 'brands/create'
-  get 'brands/edit'
-  get 'brands/update'
-  get 'brands/destroy'
-  get 'items/index'
-  get 'items/show'
-  get 'items/show'
-  get 'items/new'
-  get 'items/create'
-  get 'items/edit'
-  get 'items/update'
-  get 'items/destroy'
-  get 'orders/index'
-  get 'orders/new'
-  get 'orders/create'
-  get 'orders/edit'
-  get 'orders/update'
-  get 'orders/destroy'
-  root 'items#index'
+  resources :items
+  resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :brands, only: [:index, :new, :create, :edit, :destroy]
+  resources :orders
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
