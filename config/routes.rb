35081b86367dc27ed8f512020bfb63b5920bc8f7
hyodofumiya <f.hyodo.0123@gmail.com>
2020-03-16 12:46:51 +0900

    hamlのlink先を修正
    hamlのlink先をroutesファイルを変更したことでエラーが生じたため、修正を施した。

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :brands, only: [:index, :new, :create, :edit, :destroy]
   resources :orders
-
+  root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
