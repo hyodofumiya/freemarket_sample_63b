c37b3e45a25be56d778299cbffe09a392afcfecc
shihori711 <thremin711@gmail.com>
2020-03-21 03:42:38 +0900

    メンバーレビュー訂正３

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   get 'items/edit'
   get 'items/update'
   get 'items/destroy'
-  get 'orders/index'
+  get 'items/new'
   get 'orders/edit'
   get 'orders/update'
   get 'orders/destroy'
