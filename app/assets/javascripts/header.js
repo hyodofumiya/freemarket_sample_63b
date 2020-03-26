cea543f6a08f98bb3354308437acc903de7fd084
hyodofumiya <f.hyodo.0123@gmail.com>
2020-03-27 02:32:36 +0900

    top画面にjsを適応した

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,20 +1,29 @@
 //カーソルがカテゴリーボタンに乗った時親カテゴリーを表示
 $(function(){
-  $(".header_zone_left_btns__category").mouseover(function(){
-    $(".parent_category_list").appendTo(".header_category_list__parent");
-  });
+  $(".header_zone_left_btns__category").hover(
+    function(){
+      $(".parent_category_list").show();
+    },
+    function(){
+      $(".parent_category_list").hide();
+    }
+  );
 });
 //カーソルが親カテゴリーに乗った時子カテゴリーを表示
 $(function(){
-  $(".parent_category_name").mouseover(function(){
+  $(".parent_category_name").hover(function(){
     child = ($(this).children("ul"));
-    child.appendTo(".header_category_list__child");
+    child.show();
+  },function(){
+    child.hide();
   });
 });
 //カーソルが小カテゴリーにのった時に孫カテゴリーを表示
 $(function(){
-  $(".child_category_name").mouseover(function(){
-    grandchild = ($(this).children("ul"));
-    grandchild.appendTo(".header_category_list__grandchild");
-  })
-})
+  $(".child_category_name").hover(function(){
+    child = ($(this).children("ul"));
+    child.show();
+  },function(){
+    child.hide();
+  });
+});
