24e68a234f436b1d574741ea35f1aae40243211c
hyodofumiya <f.hyodo.0123@gmail.com>
2020-03-28 14:55:07 +0900

    top画面の見た目ホバー完成

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -2,12 +2,12 @@
 $(function(){
   $(".header_zone_left_btns__category").hover(
     function(){
+      $(this).children("a").css("color", "#2483ff").css("font-weight", "bolder");
       $(".parent_category_list").show();
-      $(this).css("color", "#2483ff");
     },
     function(){
       $(".parent_category_list").hide();
-      $(this).css("color","black");
+      $(this).children("a").css("color","").css("font-weight", "");
     }
   );
 });
@@ -15,7 +15,8 @@ $(function(){
 $(function(){
   $(".parent_category_name").hover(function(){
     child = $(this).children("ul");
-    $(this).children("a").css("color", "#2483ff").css("font-weight", "bold");
+    $(this).css("background-color", "#66ccff");
+    $(this).children("a").css("color", "#fff").css("font-weight", "bold");
     child.show();
   },function(){
     $(this).css("background-color", "");
@@ -27,18 +28,22 @@ $(function(){
 $(function(){
   $(".child_category_name").hover(function(){
     child = ($(this).children("ul"));
-    $(this).children("a").css("color","#66ccff").css("font-weight", "bold");    
+    $(this).css("background-color", "#cceeff");
+    //$(this).children("a").css("color","#fff").css("font-weight", "bold");    
     child.show();
   },function(){
-    $(this).children("a").css("color", "").css("font-weight", "");
+    $(this).css("background-color", "");
+    //$(this).children("a").css("color", "").css("font-weight", "");
     child.hide();
   });
 });
 //カーソルが孫カテゴリーにのった時の表示切り替え
 $(function(){
   $(".grandchild_category_name").hover(function(){
-    $(this).children("a").css("color", "#66ccff").css("font-weight", "bold");
+    $(this).css("background-color", "#cceeff");
+    //$(this).children("a").css("color", "#fff").css("font-weight", "bold");
   },function(){
-    $(this).children("a").css("color","").css("font-weight","");
+    $(this).css("background-color", "");
+    //$(this).children("a").css("color","").css("font-weight","");
   })
 })
