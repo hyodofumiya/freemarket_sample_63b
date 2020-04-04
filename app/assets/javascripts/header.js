2b58060425d27cc2ed80b4a041573da3a50c847b
hyodofumiya <f.hyodo.0123@gmail.com>
2020-04-04 12:56:34 +0900

    jsの修正
    ページ遷移時にturbolinksでreadyが動作しなくなる不具合を修正

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,6 +1,7 @@
 //カーソルがカテゴリーボタンに乗った時大カテゴリーを表示
-$(document).ready(function(){
-  $(".header_zone_left_btns__category").hover(
+$(document).on ('turbolinks:load',function(){
+  $(function(){
+    $(".header_zone_left_btns__category").hover(
     function(){
       $(this).children("a").css("color", "#2483ff").css("font-weight", "bolder");
       $(".parent_category_list").show();
@@ -8,41 +9,41 @@ $(document).ready(function(){
     function(){
       $(".parent_category_list").hide();
       $(this).children("a").css("color","").css("font-weight", "");
-    }
-  );
-});
+    });
+  });
 
-//カーソルが大カテゴリーに乗った時中カテゴリーを表示
-$(function(){
-  $(".parent_category_name").hover(function(){
-    child = $(this).children("ul");
-    $(this).css("background-color", "#66ccff");
-    $(this).children("a").css("color", "#fff").css("font-weight", "bold");
-    child.show();
-  },function(){
-    $(this).css("background-color", "");
-    $(this).children("a").css("color", "").css("font-weight", "");
-    child.hide();
+  //カーソルが大カテゴリーに乗った時中カテゴリーを表示
+  $(function(){
+    $(".parent_category_name").hover(function(){
+      child = $(this).children("ul");
+      $(this).css("background-color", "#66ccff");
+      $(this).children("a").css("color", "#fff").css("font-weight", "bold");
+      child.show();
+    },function(){
+      $(this).css("background-color", "");
+      $(this).children("a").css("color", "").css("font-weight", "");
+      child.hide();
+    });
+  });
+
+  //カーソルが中カテゴリーにのった時に小カテゴリーを表示
+  $(function(){
+    $(".child_category_name").hover(function(){
+      child = ($(this).children("ul"));
+      $(this).css("background-color", "#cceeff"); 
+      child.show();
+    },function(){
+      $(this).css("background-color", "");
+      child.hide();
+    });
+  });
+
+  //カーソルが小カテゴリーにのった時の表示切り替え
+  $(function(){
+    $(".grandchild_category_name").hover(function(){
+      $(this).css("background-color", "#cceeff");
+    },function(){
+      $(this).css("background-color", "");
+    });
   });
 });
-
-//カーソルが中カテゴリーにのった時に小カテゴリーを表示
-$(function(){
-  $(".child_category_name").hover(function(){
-    child = ($(this).children("ul"));
-    $(this).css("background-color", "#cceeff"); 
-    child.show();
-  },function(){
-    $(this).css("background-color", "");
-    child.hide();
-  });
-});
-
-//カーソルが小カテゴリーにのった時の表示切り替え
-$(function(){
-  $(".grandchild_category_name").hover(function(){
-    $(this).css("background-color", "#cceeff");
-  },function(){
-    $(this).css("background-color", "");
-  })
-})
