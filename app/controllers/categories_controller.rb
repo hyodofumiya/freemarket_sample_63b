5d01ecdbf1ecb059557c0d0e96c4564d018fad4e
hyodofumiya <f.hyodo.0123@gmail.com>
2020-04-06 00:01:42 +0900

    n+1問題修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,8 +8,9 @@ before_action :set_category ,only: [:index, :show]
   def show
     @select_category = Category.find(params[:id]) #選択されたカテゴリーを取得
     @subtree_category = @select_category.subtree #選択されたカテゴリー自身も含めた子孫をすべて取得
-    @items = Item.where(category_id: @subtree_category.ids).page(params[:page]) #Itemから@subtree_categoryに代入されたカテゴリーに属するitemをすべて取得し、25個ずつで区切る
+    @items = Item.where(category_id: @subtree_category.ids).includes(:images).page(params[:page]) #Itemから@subtree_categoryに代入されたカテゴリーに属するitemをすべて取得し、25個ずつで区切る
     @images = Image.where(item_id: @items.ids) #選択されたitemの画像を取得
+    binding.pry
   end
 
   def new
