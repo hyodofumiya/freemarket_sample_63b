b2a087b8a5f750c5667758d39cc833e4dedff3b5
hyodofumiya <f.hyodo.0123@gmail.com>
2020-04-06 00:25:03 +0900

    category/showのお気に入りボタンの表示を変更した

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,9 +8,7 @@ before_action :set_category ,only: [:index, :show]
   def show
     @select_category = Category.find(params[:id]) #選択されたカテゴリーを取得
     @subtree_category = @select_category.subtree #選択されたカテゴリー自身も含めた子孫をすべて取得
-    @items = Item.where(category_id: @subtree_category.ids).includes(:images).page(params[:page]) #Itemから@subtree_categoryに代入されたカテゴリーに属するitemをすべて取得し、25個ずつで区切る
-    @images = Image.where(item_id: @items.ids) #選択されたitemの画像を取得
-    binding.pry
+    @items = Item.where(category_id: @subtree_category.ids).includes(:images).includes(:favorites).page(params[:page]) #Itemから@subtree_categoryに代入されたカテゴリーに属するitemをすべて取得し、25個ずつで区切る
   end
 
   def new
