d2d725150bfeab187a0a95434ce11d44498a105b
hyodofumiya <f.hyodo.0123@gmail.com>
2020-04-06 01:59:20 +0900

    facatorybotが使えるように設定した

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  #factorybotを使えるようにする
+  config.include FactoryBot::Syntax::Methods
 end
+
