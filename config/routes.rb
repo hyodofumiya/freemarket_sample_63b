5e3ac7f3a845ad9d5f858b44484075c7dfe31ead
hyodofumiya <f.hyodo.0123@gmail.com>
2020-04-06 12:55:54 +0900

    クレジットカードのモデルとDBを削除しやりなおす
    クレジットカードのマイグレーションエラーが発生し、原因がわからなかったため、やり直すことにした

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update] do
     resources :favorites, only: [:index]
-    resources :credit_cards, only: [:index]
+    resources :credit_cards, only: [:index, :new, :create]
     resources :shopping_addresses, only: [:index]
     resources :items, only: [:index, :new, :create]
     resources :orders, only: [:index, :new, :create]
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_22_105745) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.date "birthday", null: false
+    t.string "phone_number"
+    t.text "photo_filepath"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
