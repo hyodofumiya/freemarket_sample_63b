4e4eb340494f0d34bd66a81146c8a3f8c1308263
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2020-04-07 00:25:59 +0900

    ブランドとカテゴリのフォームを部分テンプレートに分けた。ブランドを受け取った時に登録できる様に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params.merge(user_id: current_user.id))
+    @item.brand_id = get_brand_id
     if @item.save
       redirect_to item_path(@item)
     else
@@ -43,6 +44,10 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :discription, :size, :condition, :delivary, :area, :preparation_day, :price, images_attributes: [:photo])
   end
 
+  def brand_params
+    params.require(:item).permit(:brand).values.first
+  end
+
   def set_item
     @item = Item.find(params[:id])
   end
@@ -50,5 +55,14 @@ class ItemsController < ApplicationController
   def admin_user?#出品者以外は詳細ページへリダイレクト
     set_item
     redirect_to item_path(@item) unless @item.user == current_user
+  end
+
+  def get_brand_id
+    return nil unless brand_params
+    if brand = Brand.find_by(name: brand_params)
+      return brand.id
+    else
+      return Brand.create(name: brand_params).id
+    end
   end
 end
