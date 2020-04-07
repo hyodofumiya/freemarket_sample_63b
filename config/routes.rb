65edad9af051f2802908496cf1285d8902713945
hyodofumiya <f.hyodo.0123@gmail.com>
2020-04-08 00:38:13 +0900

    routesをmasterに合わせた

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "profile_photo", to: "users#profile_photo"
   patch "update_profile_photo", to: "users#profile_photo_update"
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show] do
     resources :categories, only: [:index, :show]
-    resource :favorite, only: [:create, :destroy]
+    resources :favorite, only: [:create, :destroy]
     resources :comments, only: [:create]
     resources :orders, only: [:new, :create]#orderはuserにも関係するがcurrent_userでとるのでitemにネストする。
   end
   root 'items#index'
-
 end
