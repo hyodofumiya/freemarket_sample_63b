9c20d4ad80f7cbbc76168900606ba11a99656950
shihori711 <thremin711@gmail.com>
2020-04-14 02:03:15 +0900

    削除ボタン実装

--- app/assets/javascripts/new_item.js
+++ app/assets/javascripts/new_item.js
@@ -9,14 +9,24 @@ $(function(){
     var fileReader = new FileReader();
     //読み込みが完了すると、srcにfileのURLを格納
     fileReader.onloadend = function() {
-      var src = fileReader.result
-      var html= `<img src="${src}" width="114" height="80">`
+      var src = fileReader.result      
+      var html= `<div class='item-image' data-image="${file.name}">
+             <div class=' item-image__content'>
+               <div class='item-image__content--icon'>
+                 <img src=${src} width="114" height="80" >
+               </div>
+             </div>
+             <div class='item-image__operetion'>
+               <div class='item-image__operetion--delete'>削除</div>
+             </div>
+           </div>`
       //image_box__container要素の前にhtmlを差し込む
       $('#image-box__container').before(html);
     }
     fileReader.readAsDataURL(file);
   });
 });
+
 $(document).on("click", '.item-image__operetion--delete', function(){
   //プレビュー要素を取得
   var target_image = $(this).parent().parent()
@@ -26,94 +36,3 @@ $(document).on("click", '.item-image__operetion--delete', function(){
   file_field.val("")
 })
 
-$(function(){
-  //DataTransferオブジェクトで、データを格納する箱を作る
-  var dataBox = new DataTransfer();
-  //querySelectorでfile_fieldを取得
-  var file_field = document.querySelector('input[type=file]')
-  //fileが選択された時に発火するイベント
-  $('#img-file').change(function(){
-    //選択したfileのオブジェクトをpropで取得
-    var files = $('input[type="file"]').prop('files')[0];
-    $.each(this.files, function(i, file){
-      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
-      var fileReader = new FileReader();
-
-      //DataTransferオブジェクトに対して、fileを追加
-      dataBox.items.add(file)
-      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
-      file_field.files = dataBox.files
-
-      var num = $('.item-image').length + 1 + i
-      fileReader.readAsDataURL(file);
-　　　 //画像が10枚になったら超えたらドロップボックスを削除する
-      if (num == 10){
-        $('#image-box__container').css('display', 'none')   
-      }
-      //読み込みが完了すると、srcにfileのURLを格納
-      fileReader.onloadend = function() {
-        var src = fileReader.result
-        var html= `<div class='item-image' data-image="${file.name}">
-                    <div class=' item-image__content'>
-                      <div class='item-image__content--icon'>
-                        <img src=${src} width="114" height="80" >
-                      </div>
-                    </div>
-                    <div class='item-image__operetion'>
-                      <div class='item-image__operetion--delete'>削除</div>
-                    </div>
-                  </div>`
-        //image_box__container要素の前にhtmlを差し込む
-        $('#image-box__container').before(html);
-      };
-      //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
-      $('#image-box__container').attr('class', `item-num-${num}`)
-    });
-  });
-  //削除ボタンをクリックすると発火するイベント
-  $(document).on("click", '.item-image__operetion--delete', function(){
-    //プレビュー要素を取得
-    var target_image = $(this).parent().parent()
-    //プレビューを削除
-    target_image.remove();
-    //inputタグに入ったファイルを削除
-    file_field.val("")
-  })
-});
-
-
-
-// $(document).on('turbolinks:load', ()=> {
-//   // 画像用のinputを生成する関数
-//   const buildFileField = (index)=> {
-//     const html = `<div data-index="${index}" class="js-file_group">
-//                     <input class="js-file" type="file"
-//                     name="item[image_forms_attributes][${index}][src]"
-//                     id="item_image_forms_attributes_${index}_src"><br>
-//                     <div class="js-remove">削除</div>
-//                   </div>`;
-//     return html;
-//   }
-//    // プレビュー用のimgタグを生成する関数
-//    const buildImg = (index, url)=> {
-//     const html = `<img data-index="${index}" src="${url}" width="50px" height="50px">`;
-//     return html;
-//   }
-
-//   // file_fieldのnameに動的なindexをつける為の配列
-//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
-//   $('#image-box').on('change', '.js-file', function(e) {
-//     // fileIndexの先頭の数字を使ってinputを作る
-//     $('#image-box').append(buildFileField(fileIndex[0]));
-//     fileIndex.shift();
-//     // 末尾の数に1足した数を追加する
-//     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-//   });
-
-//   $('#image-box').on('click', '.js-remove', function() {
-//     $(this).parent().remove();
-//     // 画像入力欄が0個にならないようにしておく
-//     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-//   });
-// });
