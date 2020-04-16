c7a5ed6c3543cf0681ced56a09c5b47de63d458f
hyodofumiya <f.hyodo.0123@gmail.com>
2020-04-16 16:40:52 +0900

    カテゴリーおよびサイズ情報の入力フォーム完成

--- app/assets/javascripts/item_new.js
+++ app/assets/javascripts/item_new.js
@@ -1,29 +1,33 @@
-function buildCategoryForm(select, form_id){
+function buildCategoryForm(select, form_id){                                  //jsonで受け取ったparamsをselectとして中身を順にセレクトボックスに追加していく
   $.each(select, function(index, val){
     var html = `<option value="${val.id}">${val.name}</option>`
-    console.log(html);
-    console.log(form_id);
     $(form_id).append(html);
   });
-}
+};
 
+function hideSizeForm(){                                                      //サイズ選択のフォームを非表示にして選択不可能にする
+  $('.main__showmain__detail__size').hide();
+  $('.main__showmain__detail__sizearea').hide();
+  document.getElementById('form_size_area').setAttribute("disabled", "true");
+}
 
 $(document).on ('turbolinks:load',function(){
   //大カテゴリーが選択された時の動作
-  $(function(){
-    $("#category_parent_area").click(function() { //大カテゴリーの選択ボックスをクリックすると選択肢が追加される
+  $(function(){ 
+    $("#category_parent_area").click(function() {                             //大カテゴリーの選択ボックスをクリックすると選択肢が追加される
+      hideSizeForm();
       $.each(gon.roots, function(index, val){
         var html = `<option value="${val.id}">${val.name}</option>`
         $("#category_parent_area").append(html);
       });
     });
-    $("#category_parent_area").change(function(){
-      $("#category_child_area").empty();
-      $(".main__showmain__detail__categoryarea__grandchild").hide();
-      $("#category_grandchild_area").empty();
-      var parentId = $(this).val();
-      var url = location.href;
-      $.ajax({
+    $("#category_parent_area").change(function(){                           //大カテゴリーの内容が変更された時、中カテゴリーの中身を削除する
+      $("#category_child_area").empty();                                    //中カテゴリーフォームの中身を削除
+      $(".main__showmain__detail__categoryarea__grandchild").hide();        //小カテゴリーを非表示にする
+      $("#category_grandchild_area").empty();                               //小カテゴリーフォームの中身を削除
+      var parentId = $(this).val();                                         //選択された大カテゴリーのidを”parentID”として宣言
+      var url = location.href;                                              //ajaxで戻る時に必要なurlを”url”として宣言
+      $.ajax({                                                              //ajaxで交信
         url: url,
         type: "GET",
         data: {category_id: parentId},
@@ -33,7 +37,7 @@ $(document).on ('turbolinks:load',function(){
         $('#category_child_area').append(`<option value="">---選択してください---</option>`)
         $('.main__showmain__detail__categoryarea__child').show();
         $('#category_grandchild_area').append(`<option value="">---選択してください---</option>`)
-        var html = buildCategoryForm(p.child, "#category_child_area");
+        var html = buildCategoryForm(p.child, "#category_child_area");        //ajaxで送られてきた値を元に中カテゴリーフォームの選択肢を作成
       })
       .fail(function(){
         alart("エラー");
@@ -44,6 +48,7 @@ $(document).on ('turbolinks:load',function(){
   //中カテゴリーが選択された時の動作
   $(function(){
     $("#category_child_area").change(function(){
+      hideSizeForm();
       $("#category_grandchild_area").empty();
       var childId = $(this).val();
       var url = location.href;
@@ -65,5 +70,27 @@ $(document).on ('turbolinks:load',function(){
   });
 
   //小カテゴリーが選択された時の動作
-  
+  $(function(){
+    $('#category_grandchild_area').change(function(){
+      var grandchildId = $(this).val();
+      var url = location.href;
+      $.ajax({
+        url: url,
+        type: "GET",
+        data: {category_id: grandchildId},
+        dataType: 'json',
+      })
+      .done(function(g){                                                        //ajaxで小カテゴリーで選択されたカテゴリー情報を受け取る
+        var selfcategory = g.selfcategory                                       //小カテゴリーで選択したカテゴリーの情報を"selfcategory"として宣言
+        if (selfcategory.size){                                                 //小カテゴリーで選択したカテゴリーがsize:trueだった場合、サイズ入力フォームを表示させる
+          $('.main__showmain__detail__size').show();
+          $('.main__showmain__detail__sizearea').show();
+          document.getElementById('form_size_area').removeAttribute("disabled");
+        };
+      })
+      .fail(function(){
+        alart("エラー");
+      })
+    });
+  });
 });
--- app/views/items/new.json.jbuilder
+++ app/views/items/new.json.jbuilder
@@ -1,2 +1,3 @@
 json.category_id params[:category_id]
 json.child @category.find(params[:category_id]).children
+json.selfcategory @category.find(params[:category_id])
