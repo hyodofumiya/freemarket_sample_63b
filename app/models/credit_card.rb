89e2752534f05641e2169fcfa8abc92251d4a519
hyodofumiya <f.hyodo.0123@gmail.com>
2020-04-16 19:13:29 +0900

    クレジットカードモデルのuserバリデーションを削除した

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -2,6 +2,5 @@ class CreditCard < ApplicationRecord
   belongs_to :user
   has_many :orders
 
-  validates :user_id, uniqueness: true
   validates :card_id, :customer_id, uniqueness:true, presence: true
 end
