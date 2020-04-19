c038ece34cb89e82ddb067b4f1ec89ba52603ce8
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2020-04-19 12:34:20 +0900

    fix deploy setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.12.0"
+lock "~> 3.13.0"
 
 set :application, "freemarket_sample_63b"
 set :repo_url, "git@github.com:hyodofumiya/freemarket_sample_63b.git"
